6caf0faf78946114e1255fb9cfcb93cf9602b3df
TaroFujinoki <tarofujinoki1115@gmail.com>
2019-11-27 15:38:40 +0900

    エラーを修正

--- score_count.py
+++ score_count.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 def count_score(a):
     score = 0
-    flame = [10]
+    flame = [0] * 10
     flame_count = 0 
     throw_count = 0
     while flame_count < 10:
         if a[throw_count] == 10:
-            score +=  a[throw_count] + a[throw_count + 1] + a[throw_count + 2]; "フレームの計算"
+            score +=  a[throw_count] + a[throw_count + 1] + a[throw_count + 2]
             flame[flame_count] = score
             flame_count += 1
             throw_count += 1
@@ -23,6 +23,9 @@ def count_score(a):
             throw_count += 2
     print(score)
     print(flame)
+
 def test(a):
-    count_score([10,8,2,10,0,10,10,6,4,10,8,2,10,9,1,10])
-    print("a")
+    count_score(a)
+
+if __name__ == "__main__":
+    test([10,8,2,10,0,10,10,6,4,10,8,2,10,9,1,10])
